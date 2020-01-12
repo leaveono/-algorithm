3ff1257319e162b9e298fc17c096a85dea133de5
leaveono <2543806295@qq.com>
2020-01-12 22:31:35 +0800

    MoveZeros

---
+++ MoveZeros.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leaveone
+ * Date: 2020/1/12
+ * Time: 22:30
+ */
+
+class MoveZeros
+{
+    //method1
+    //时间复杂度O(n)
+    //空间复杂度O(1)
+    function moveZeroes(&$nums) {
+        for ($i = 0;$i< count($nums);$i++){
+            if($nums[$i] == 0){
+                unset($nums[$i]);
+                array_push($nums,0);
+            }
+        }
+        //foreach
+//            foreach($nums as $key => $val){
+//                if($nums[$key] == 0){
+//                    unset($nums[$key]);
+//                    $nums[] = 0;
+//                }
+//            }
+    }
+    //method2
+    //时间复杂度O(n)
+    //空间复杂度O(1)
+//        function moveZeroes(&$nums) {
+//            $len = count($nums);
+//            $index = 0;
+//            foreach ($nums as $key => $val){
+//                if($val != 0){
+//                    $nums[$index] = $nums[$key];
+//                    $index++;
+//                }
+//            }
+//            for (;$index < $len;$index++){
+//                $nums[$index] = 0;
+//            }
+//        }
+}
